5a55925faa9d80c0b62cd0250e4bffe7d6ab7469
Chris Pondoc <chris.pondoc@comcast.net>
2025-04-09 17:12:03 -0700

    Updated

--- rerank.py
+++ rerank.py
@@ -9,9 +9,9 @@ from sentence_transformers import SentenceTransformer
 # Define reranker, set of base models that we can change
 BASE_MODEL = "Snowflake/snowflake-arctic-embed-m"
 QUALITY_MODELS = {
-    # "HuggingFaceTB/fineweb-edu-classifier": "fineweb-edu",
+    "HuggingFaceTB/fineweb-edu-classifier": "fineweb",
     # "gpt2": "gpt2",
-    "nvidia/domain-classifier": "nvidia-dc",
+    # "nvidia/domain-classifier": "nvidia-dc",
 }
 
 # Also define the set of tasks
@@ -19,24 +19,22 @@ TASKS = ["NFCorpus"]
 tasks = mteb.get_tasks(tasks=TASKS, languages=["eng"])
 
 # Iterate through each model, set up the initial encoder
-for quality_p in [0.99]:
-    for key, value in QUALITY_MODELS.items():
-        dual_encoder = SentenceTransformer(BASE_MODEL).to('cuda' if torch.cuda.is_available() else 'cpu')
-        eval_splits = ["test"]
+for subset_size in [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 10000, 10771]:
+    for quality_p in [0.995, 0.99, 0.9, 0.8, 0.7, 0.6, 0.99]:
+        for key, value in QUALITY_MODELS.items():
+            dual_encoder = SentenceTransformer(BASE_MODEL).to('cuda' if torch.cuda.is_available() else 'cpu')
+            eval_splits = ["test"]
 
-        # Run eval by first doing the results, then reranking
-        for task in TASKS:
-            evaluation = MTEB(tasks=[task])
-            evaluation.run(
-                dual_encoder,
-                eval_splits=eval_splits,
-                save_predictions=True,
-<<<<<<< HEAD
-                output_folder="noisy_results/rerank-results/5000/" + value + "/" + str(quality_p) + "/",
-=======
-                output_folder="noisy_results/test/10771/" + value + "/" + str(quality_p) + "/",
->>>>>>> 9c378c46d03497c729393f53977088348108d6f5
-                quality_p=quality_p,
-                quality_classifier=key,
-                classifier_normalization="softmax_entropy",
-            )
+            # Run eval by first doing the results, then reranking
+            for task in TASKS:
+                evaluation = MTEB(tasks=[task])
+                evaluation.run(
+                    dual_encoder,
+                    eval_splits=eval_splits,
+                    save_predictions=True,
+                    output_folder=f"calibration/{value}/{str(quality_p)}/{str(subset_size)}/",
+                    subset_size=subset_size,
+                    quality_p=quality_p,
+                    quality_classifier=value,
+                    classifier_normalization="softmax_entropy",
+                )
